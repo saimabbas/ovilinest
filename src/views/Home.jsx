51522ec40879af7d9427507304e5e892c0883d49
Saim Abbas <saimabbas220022@gmail.com>
2022-12-21 20:44:13 +0500

    scale

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -245,10 +245,10 @@ const Home = () => {
     homeLandingAnim.fromTo(
       ".home-page",
       {
-        opacity: "0",
+        scale: "0",
       },
       {
-        opacity: "1",
+        scale: "1",
         duration: 1,
       }
     );
