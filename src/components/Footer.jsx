1b815082ef8a3bfcc768d9732c7f6ac1262017ad
Saim Abbas <saimabbas220022@gmail.com>
2023-01-20 02:19:29 +0500

    Changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,6 +20,13 @@ const Footer = () => {
             <div></div>
             <span>제휴문의 ovilinest@yongpyong.co.kr</span>
           </div>
+          <div className="footer-top mt-3">
+            <span> 용평리조트 대표이사 : 신달순, 임학운</span>
+            <div></div>
+            <span>
+              주소 : 강원도 평창군 대관령면 올림픽로 715(구 용산리 130)
+            </span>
+          </div>
           <p>Copyright 2022 YONGPYONG RESORT All right reserved.</p>
         </div>
       </div>
