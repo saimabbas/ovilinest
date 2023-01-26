3cdff6a9a9ddd640ec4457f2a213cfebee46f7d3
Saim Abbas <saimabbas220022@gmail.com>
2023-01-26 18:47:16 +0500

    10px

--- src/views/MyNFT.jsx
+++ src/views/MyNFT.jsx
@@ -104,17 +104,6 @@ const MyNFT = () => {
       );
     }, 2000);
   };
-  $(function () {
-    var w = Math.max(
-      document.documentElement.clientWidth,
-      window.innerWidth || 0
-    );
-    var h = Math.max(
-      document.documentElement.clientHeight,
-      window.innerHeight || 0
-    );
-    $("html, body").css({ width: w, height: h });
-  });
 
   return (
     <div className="app">
